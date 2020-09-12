d75f12eea843ec25f59ccff0a40ec507438d8d2b
ninjachicks <carolin.bruederle@gmail.com>
2020-09-12 17:00:00 +0200

    adds view index.php

---
+++ resources/views/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ninjatracker</title>
+    </head>
+
+    <body>
+
+        <div>
+            ninjatracker platform
+        </div>
+
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+       
+
+    </body>
+</html>
