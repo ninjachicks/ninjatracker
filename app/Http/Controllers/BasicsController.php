e99e88ffdec5f027bec4aadd9e8ba787d364fad9
ninjachicks <carolin.bruederle@gmail.com>
2020-09-12 16:57:24 +0200

    adds basic controllers index, login, register

---
+++ app/Http/Controllers/BasicsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BasicsController extends Controller
+{
+    public function index() {
+        return view("home_index");
+    }
+
+    public function login() {
+        return view("home_login");
+    }
+
+    public function register() {
+        return view("home_register");
+    }
+
+}
