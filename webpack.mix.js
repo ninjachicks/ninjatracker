9a7744bc8c8d4b7477fa8244020b537dbf158270
ninjachicks <carolin.bruederle@gmail.com>
2020-09-13 15:53:37 +0200

    adds new css routes

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+    .postCss('resources/css/ninjatracker_main.css', 'public/css', [
+        //
+    ])
+    .postCss('resources/css/category.css', 'public/css', [
         //
     ]);
